d379d54cba429470bf6a2901ce8f50d2a0221fd7
latototita <pearlmartbusinesses@gmail.com>
2022-08-08 03:27:08 -0500

    Laban

--- main.py
+++ main.py
@@ -3,17 +3,16 @@ from kivy.uix.widget import Widget
 from kivy.clock import Clock
 from jnius import autoclass
 from android.runnable import run_on_ui_thread
- 
+
 WebView = autoclass('android.webkit.WebView')
 WebViewClient = autoclass('android.webkit.WebViewClient')
 #activity = autoclass('org.renpy.android.PythonActivity').mActivity
 activity = autoclass('org.kivy.android.PythonActivity').mActivity
- 
+
 class Wv(Widget):
     def __init__(self, **kwargs):
         super(Wv, self).__init__(**kwargs)
         Clock.schedule_once(self.create_webview, 0)
- 
 @run_on_ui_thread
 def create_webview(self, *args):
     webview = WebView(activity)
@@ -32,5 +31,5 @@ class ServiceApp(App):
     def build(self):
         return Wv()
  
-if __name__ == '__main__':
+if __name__ == "__main__":
     ServiceApp().run()
