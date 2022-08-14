a30ebfcc825fbce167ff1b0b405d8094fa8c60e5
latototita <pearlmartbusinesses@gmail.com>
2022-08-14 15:52:46 +0000

    Laban

--- .buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/other_builds/openssl/arm64-v8a__ndk_target_21/openssl1.1/crypto/buildinf.h
+++ .buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/other_builds/openssl/arm64-v8a__ndk_target_21/openssl1.1/crypto/buildinf.h
@@ -11,7 +11,7 @@
  */
 
 #define PLATFORM "platform: android-arm64"
-#define DATE "built on: Wed Aug 10 13:08:29 2022 UTC"
+#define DATE "built on: Sun Aug 14 15:48:00 2022 UTC"
 
 /*
  * Generate compiler_flags as an array of individual characters. This is a
--- .buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/other_builds/openssl/armeabi-v7a__ndk_target_21/openssl1.1/crypto/buildinf.h
+++ .buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/other_builds/openssl/armeabi-v7a__ndk_target_21/openssl1.1/crypto/buildinf.h
@@ -11,7 +11,7 @@
  */
 
 #define PLATFORM "platform: android-arm"
-#define DATE "built on: Wed Aug 10 12:55:05 2022 UTC"
+#define DATE "built on: Sun Aug 14 15:45:33 2022 UTC"
 
 /*
  * Generate compiler_flags as an array of individual characters. This is a
